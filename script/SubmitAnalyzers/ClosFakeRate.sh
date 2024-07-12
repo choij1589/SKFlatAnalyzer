65a6c120ae3b9dbcc8c4fec5bc0fc90c68499f3b
Jin Choi <jin.choi@cern.ch>
2024-07-12 14:45:11 +0900

    reorganizing TriLepton sample submission

--- script/SubmitAnalyzers/ClosFakeRate.sh
+++ script/SubmitAnalyzers/ClosFakeRate.sh
@@ -1,8 +1,9 @@
-SKFlat.py -a ClosFakeRate -i TTLL_powheg --skim SkimTree_SS2lOR3l -n 20 -e 2016preVFP --userflags Skim1E2Mu,FakeStudy --python &
-SKFlat.py -a ClosFakeRate -i TTLL_powheg --skim SkimTree_SS2lOR3l -n 20 -e 2016preVFP --userflags Skim3Mu,FakeStudy --python &
-SKFlat.py -a ClosFakeRate -i TTLL_powheg --skim SkimTree_SS2lOR3l -n 20 -e 2016postVFP --userflags Skim1E2Mu,FakeStudy --python &
-SKFlat.py -a ClosFakeRate -i TTLL_powheg --skim SkimTree_SS2lOR3l -n 20 -e 2016postVFP --userflags Skim3Mu,FakeStudy --python &
-SKFlat.py -a ClosFakeRate -i TTLL_powheg --skim SkimTree_SS2lOR3l -n 30 -e 2017 --userflags Skim1E2Mu,FakeStudy --python &
-SKFlat.py -a ClosFakeRate -i TTLL_powheg --skim SkimTree_SS2lOR3l -n 30 -e 2017 --userflags Skim3Mu,FakeStudy --python &
-SKFlat.py -a ClosFakeRate -i TTLL_powheg --skim SkimTree_SS2lOR3l -n 40 -e 2018 --userflags Skim1E2Mu,FakeStudy --python &
-SKFlat.py -a ClosFakeRate -i TTLL_powheg --skim SkimTree_SS2lOR3l -n 40 -e 2018 --userflags Skim3Mu,FakeStudy --python &
+#!/bin/bash
+SKFlat.py -a ClosFakeRate -i TTLL_powheg --skim SkimTree_SS2lOR3l -n 20 -e 2016preVFP --userflags Skim1E2Mu --python &
+SKFlat.py -a ClosFakeRate -i TTLL_powheg --skim SkimTree_SS2lOR3l -n 20 -e 2016preVFP --userflags Skim3Mu --python &
+SKFlat.py -a ClosFakeRate -i TTLL_powheg --skim SkimTree_SS2lOR3l -n 20 -e 2016postVFP --userflags Skim1E2Mu --python &
+SKFlat.py -a ClosFakeRate -i TTLL_powheg --skim SkimTree_SS2lOR3l -n 20 -e 2016postVFP --userflags Skim3Mu --python &
+SKFlat.py -a ClosFakeRate -i TTLL_powheg --skim SkimTree_SS2lOR3l -n 30 -e 2017 --userflags Skim1E2Mu --python &
+SKFlat.py -a ClosFakeRate -i TTLL_powheg --skim SkimTree_SS2lOR3l -n 30 -e 2017 --userflags Skim3Mu --python &
+SKFlat.py -a ClosFakeRate -i TTLL_powheg --skim SkimTree_SS2lOR3l -n 40 -e 2018 --userflags Skim1E2Mu --python &
+SKFlat.py -a ClosFakeRate -i TTLL_powheg --skim SkimTree_SS2lOR3l -n 40 -e 2018 --userflags Skim3Mu --python &
